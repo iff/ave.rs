32d1514aa6a8110bc160d353b2dc7a9936fc2467
Yves Ineichen <iff@yvesineichen.com>
2023-10-28 15:18:23 +0200

    ..

--- src/types.rs
+++ src/types.rs
@@ -1,8 +1,9 @@
 use firestore::FirestoreTimestamp;
 use serde::{Deserialize, Serialize};
 
-trait toPk {
-    fn as_str(&self) -> String;
+/// converts to database primary key
+trait Pk {
+    fn toPk(&self) -> String;
 }
 
 type ObjId = String;
@@ -24,8 +25,8 @@ pub enum ObjectId {
 }
 
 // TODO parsing?
-impl toPk for ObjectId {
-    fn as_str(&self) -> String {
+impl Pk for ObjectId {
+    fn toPk(&self) -> String {
         match self {
             BaseObjectId(objId) => objId,
             ReleaseObjectId(objId, revId) => objId + "/release/" + revId,
@@ -41,4 +42,33 @@ pub struct Patch {
     pub authorId: ObjId,
     pub createdAt: firestore::FirestoreTimestamp,
     pub operation: Operation,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub enum Operation<T> {
+    /// applied to objects for adding, updating and inserting multiple elements in a single op
+    Set { path: Path, value: Option<T> },
+
+    /// manipulate arrays (remove, insert multiple elements in a single op
+    Splice {
+        path: Path,
+        index: u32,
+        remove: u32,
+        insert: std::Vec<T>,
+    },
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Object {
+    id: ObjId,
+    objectType: String,
+    created_at: firestore::FirestoreTimestamp,
+    created_by: ObjId,
+    deleted: Option<bool>,
+}
+
+impl Pk for Object {
+    fn toPk(&self) -> String {
+        self.id
+    }
 }
