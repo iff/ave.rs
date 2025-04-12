06e1d5dab6682eb87318d89c7c96b9922963a0f2
Yves Ineichen <iff@yvesineichen.com>
2025-04-12 16:03:03 +0200

    add generic create object

--- bin/all-o-stasis/src/storage.rs
+++ bin/all-o-stasis/src/storage.rs
@@ -5,7 +5,7 @@ use firestore::{path_camel_case, FirestoreQueryDirection, FirestoreResult};
 use futures::stream::BoxStream;
 use futures::TryStreamExt;
 use otp::types::{Object, ObjectId, ObjectType, Operation, Patch, RevId, Snapshot};
-use otp::{apply, rebase, ZERO_REV_ID};
+use otp::{apply, rebase, ROOT_OBJ_ID, ROOT_PATH, ZERO_REV_ID};
 use serde_json::{from_value, Value};
 
 use crate::routes::{LookupObjectResponse, PatchObjectResponse};
@@ -95,10 +95,52 @@ async fn store_snapshot(
     Ok(p)
 }
 
+pub(crate) async fn create_object(
+    state: &AppState,
+    gym: &String,
+    author_id: ObjectId,
+    object_type: ObjectType,
+    value: Value,
+) -> Result<Object, AppError> {
+    let parent_path = state.db.parent_path("gyms", gym)?;
+    let obj = Object::new(object_type, ROOT_OBJ_ID.to_owned());
+    let obj: Option<Object> = state
+        .db
+        .fluent()
+        .insert()
+        .into(OBJECTS_COLLECTION)
+        .generate_document_id()
+        .parent(&parent_path)
+        .object(&obj)
+        .execute()
+        .await?;
+
+    let obj = obj.ok_or_else(AppError::Query)?;
+
+    let op = Operation::Set {
+        path: ROOT_PATH.to_string(),
+        value: Some(value.clone()),
+    };
+    let patch = Patch {
+        object_id: obj.id(),
+        revision_id: ZERO_REV_ID,
+        author_id,
+        created_at: None,
+        operation: op,
+    };
+    let patch = store_patch(&state, &gym, &patch).await?;
+    let _ = patch.ok_or_else(AppError::Query)?;
+
+    update_view(state, gym, &obj.id(), &value).await?;
+
+    Ok(obj)
+}
+
 pub(crate) async fn update_view(
     state: &AppState,
     gym: &String,
-    snapshot: &Snapshot,
+    object_id: &ObjectId,
+    content: &Value,
 ) -> Result<(), AppError> {
     let parent_path = state.db.parent_path("gyms", gym)?;
 
@@ -110,36 +152,36 @@ pub(crate) async fn update_view(
         .by_id_in(OBJECTS_COLLECTION)
         .parent(&parent_path)
         .obj()
-        .one(&snapshot.object_id)
+        .one(&object_id)
         .await?
         .ok_or(AppError::Query())?;
 
     match obj.object_type {
         ObjectType::Account => {
-            let account = from_value::<Account>(snapshot.content.clone())
-                .map_err(|e| AppError::ParseError(format!("{} in: {}", e, snapshot.content)))?;
+            let account = from_value::<Account>(content.clone())
+                .map_err(|e| AppError::ParseError(format!("{} in: {}", e, content)))?;
 
             let _: Option<Account> = state
                 .db
                 .fluent()
                 .update()
                 .in_col(ACCOUNTS_VIEW_COLLECTION)
-                .document_id(snapshot.object_id.clone())
+                .document_id(object_id.clone())
                 .parent(&parent_path)
                 .object(&account)
                 .execute()
                 .await?;
         }
         ObjectType::Boulder => {
-            let boulder = from_value::<Boulder>(snapshot.content.clone())
-                .map_err(|e| AppError::ParseError(format!("{} in: {}", e, snapshot.content)))?;
+            let boulder = from_value::<Boulder>(content.clone())
+                .map_err(|e| AppError::ParseError(format!("{} in: {}", e, content)))?;
 
             let _: Option<Boulder> = state
                 .db
                 .fluent()
                 .update()
                 .in_col(BOULDERS_VIEW_COLLECTION)
-                .document_id(snapshot.object_id.clone())
+                .document_id(object_id.clone())
                 .parent(&parent_path)
                 .object(&boulder)
                 .execute()
@@ -498,7 +540,7 @@ async fn save_operation(
         .ok_or_else(AppError::Query)?;
 
     // TODO moved to here
-    update_view(state, gym, &new_snapshot).await?;
+    update_view(state, gym, &new_snapshot.object_id, &new_snapshot.content).await?;
 
     let patch = Patch {
         object_id,
