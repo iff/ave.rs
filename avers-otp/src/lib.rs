91ff52c7562b5131e7d574f4a2088048fc35d9e0
Yves Ineichen <iff@yvesineichen.com>
2025-07-27 10:27:37 +0200

    operation: move out

--- avers-otp/src/lib.rs
+++ avers-otp/src/lib.rs
@@ -1,5 +1,7 @@
+pub mod operation;
 pub mod patching;
 pub mod types;
 
+pub use crate::operation::{Operation, OperationError};
 pub use crate::patching::{apply, rebase, PatchError};
-pub use crate::types::{Object, Operation, OtError, ZERO_REV_ID};
+pub use crate::types::{Object, ZERO_REV_ID};
