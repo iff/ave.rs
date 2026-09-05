0ea0acf92a316b70658e8799f6022ddb507ed18f
Yves Ineichen <iff@yvesineichen.com>
2026-09-05 16:36:37 +0200

    restrict stats to setters and admins

--- bin/all-o-stasis/src/routes/stats.rs
+++ bin/all-o-stasis/src/routes/stats.rs
@@ -4,10 +4,16 @@ use axum::{
     response::Json,
     routing::get,
 };
+use axum_extra::extract::CookieJar;
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
-use crate::{AppError, AppState, types::BouldersView};
+use crate::{
+    AppError, AppState,
+    passport::author_from_session,
+    routes::api::account_role,
+    types::{AccountRole, BouldersView},
+};
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -26,7 +32,18 @@ pub fn routes() -> Router<AppState> {
 async fn stats_boulders(
     State(state): State<AppState>,
     Path(gym): Path<String>,
+    jar: CookieJar,
 ) -> Result<Json<Vec<BoulderStat>>, AppError> {
+    // restrict stats for cost reasons to admins and setters
+    // TODO fix stat computation (checkpointing) to make the query cheaper.
+    // currently each stat call retrieves lots of documents.
+    let session_id = jar.get("session").ok_or(AppError::NoSession())?;
+    let created_by = author_from_session(&state, &gym, session_id).await?;
+    let role = account_role(&state, &gym, &created_by).await?;
+    if AccountRole::User == role {
+        return Err(AppError::NotAuthorized());
+    }
+
     let transform_date = |epoch_millis: usize| {
         let date = if let Some(date) =
             DateTime::from_timestamp_millis(epoch_millis as i64)
